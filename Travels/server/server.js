09bcbaead13e3972b1d21850b02eaf07f9c1df35
smdnipun <smdnipun@gmail.com>
2022-10-28 06:46:24 +0530

    backend recode

--- Travels/server/server.js
+++ Travels/server/server.js
@@ -4,32 +4,38 @@ import dotenv from 'dotenv'
 import cors from 'cors'
 
 import userRoute from '../server/routes/user.routes.js'
-
-
-const app = express()
 dotenv.config()
+const app = express()
+const port = process.env.PORT || 5000
 
-const connect = async () => {
-  try {
-    await mongoose.connect(process.env.MONG_URL).then(() => {
-      console.log('Connected to mongoDB')
-    })
-  } catch (err) {
-    throw err
-  }
-}
-
+app.use(cors())
 app.use(express.json())
-// app.use(cors())
+app.use('/user', userRoute)
 
-app.use('/user',userRoute)
+const uri = process.env.MONG_URL
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
 
-mongoose.connection.on('disconnected', () => {
+// const connect = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONG_URL).then(() => {
+//       console.log('Connected to mongoDB')
+//     })
+//   } catch (err) {
+//     throw err
+//   }
+// }
+
+const connection = mongoose.connection
+connection.once('open', () => {
+  console.log('MongoDB database connection established successfully')
+})
+
+connection.on('disconnected', () => {
   console.log('mongoDB disconnected !!!')
 })
 
 //port connecting
-app.listen(process.env.PORT, () => {
-  connect()
+app.listen(port, () => {
+  // connect()
   console.log('Successfully connected to PORT 5000')
 })
